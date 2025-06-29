4b417c1b4285bf977cf559072dccb81efd2937a5
ygggg50 <2786855627@qq.com>
2025-06-29 23:51:26 +0800

    Add files via upload

---
+++ 4.2.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+    double high;
+    int n;
+    scanf("%lf,%d", &high, &n);
+
+    double distance = high;
+    double heigh;
+
+    for (int i = 1; i < n; i++)
+    {
+        high /= 2;
+        distance += high * 2;
+    }
+
+    heigh = high / 2;
+
+    printf("%.1lf,%.1lf", distance, heigh);
+
+    return 0;
+}
